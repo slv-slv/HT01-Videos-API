3a67ac4b73963b6d1fb126ac4c8ab2fae0e12325
Viacheslav Solovev <slvsl.spb@gmail.com>
2024-12-11 00:29:14 +0300

    added deleteVideo function

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import {SETTINGS} from './settings';
 import {videosRouter} from './videos';
+import { db } from './db/db';
 
 export const app = express();
 app.use(express.json());
@@ -12,3 +13,8 @@ app.get('/', (req, res) => {
 });
 
 app.use(SETTINGS.PATH.VIDEOS, videosRouter);
+
+app.delete('/testing/all-data', (req, res) => {
+    db.videos = [];
+    res.status(204).end();
+});
---
+++ src/videos/deleteVideo.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import { db } from '../db/db';
+
+export const deleteVideo = (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  const dbIndex = db.videos.findIndex((video) => video.id === id);
+  if (dbIndex === -1) {
+      res.status(404).json({ message: 'Video not found' });
+  }
+
+  db.videos.splice(dbIndex, 1);
+  res.status(204).end();
+};
