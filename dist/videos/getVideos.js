136bf0baaef27e43986b279200c9a970f69fdf59
Viacheslav Solovev <slvsl.spb@gmail.com>
2024-12-11 16:33:01 +0300

    Removed dist folder from tracking and update

--- dist/videos/getVideos.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getVideos = void 0;
-const db_1 = require("../db/db");
-const getVideos = (req, res) => {
-    const videos = db_1.db.videos;
-    res.status(200).json(videos);
-};
-exports.getVideos = getVideos;
